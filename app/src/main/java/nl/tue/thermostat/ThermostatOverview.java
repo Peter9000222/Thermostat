cf802253358986efb1c10b9bc77385ab8e837afc
peter9000222 <p.g.r.m.j.gerards@student.tue.nl>
2016-06-11 13:21:10 +0200

    working program switch communicates with server

--- app/src/main/java/nl/tue/thermostat/ThermostatOverview.java
+++ app/src/main/java/nl/tue/thermostat/ThermostatOverview.java
@@ -14,31 +14,42 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import org.thermostatapp.util.*;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ThermostatOverview extends AppCompatActivity {
 
     int stemp=200;
     TextView temp;
-    TextView program;
     SeekBar tempbar;
     View view;
-    Button bweekprogram;
-    Boolean on = false;
+    Button bweekprogram, bsetprogram;
+    Boolean on;                 // state is set by thread for program button
     Drawable weekon;
     Drawable weekoff;
+
+    //time
+    TimerTask taskTime;
+    String getParamTime;
+    int clockTime = 1000;
+    TextView currentTime;
+
+    //program switch
+    String getParamProgram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        HeatingSystem.BASE_ADDRESS = "http://wwwis.win.tue.nl/2id40-ws/19";
-
-
         setContentView(R.layout.activity_thermostat_overview);
-        Button bplus = (Button) findViewById(R.id.bplus);
-        Button bminus = (Button) findViewById(R.id.bminus);
+        // server addresses
+        HeatingSystem.BASE_ADDRESS = "http://wwwis.win.tue.nl/2id40-ws/19";
+        HeatingSystem.WEEK_PROGRAM_ADDRESS = HeatingSystem.BASE_ADDRESS + "/weekProgram";
+
+        // temp display
         temp = (TextView) findViewById(R.id.temp);
-        tempbar = (SeekBar) findViewById(R.id.tempbar);
-        final Button bsetprogram = (Button) findViewById(R.id.bsetprogram);
+
+        // set programs button
+        bsetprogram = (Button) findViewById(R.id.bsetprogram);
         bsetprogram.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -46,6 +57,9 @@ public class ThermostatOverview extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // temp bar
+        tempbar = (SeekBar) findViewById(R.id.tempbar);
         tempbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int  progress, boolean fromUser) {
@@ -66,6 +80,8 @@ public class ThermostatOverview extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}});
 
+        // temp + button
+        Button bplus = (Button) findViewById(R.id.bplus);
         bplus.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -84,6 +100,8 @@ public class ThermostatOverview extends AppCompatActivity {
             }
         });
 
+        // temp - button
+        Button bminus = (Button) findViewById(R.id.bminus);
         bminus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -97,27 +115,99 @@ public class ThermostatOverview extends AppCompatActivity {
                 tempbar.setProgress(stemp);
             }
         });
+
+        // week program button
         bweekprogram = (Button) findViewById(R.id.bweekprogram);
         Resources res = getResources();
-        weekon = res.getDrawable(R.drawable.on);
-        weekoff = res.getDrawable(R.drawable.off);
+        weekon = res.getDrawable(R.drawable.on);                                 // layout on
+        weekoff = res.getDrawable(R.drawable.off);                               // layout off
         bweekprogram.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (on == false) {
-                    bweekprogram.setText("Week program on");
-                    bweekprogram.setBackground(weekon);
+                if (on == false) {                                               // current state of week program is off
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                        try {
+                          HeatingSystem.put("weekProgramState", "on");           // set program state to on
+                        } catch (Exception e) {
+                            System.err.println("Error from getdata " + e);
+                        }
+                    }}).start();
+                    bweekprogram.setText("Week program on");                     // set program button text to "off"
+                    bweekprogram.setBackground(weekon);                          // set program button layout to on
                     on = true;
                 }
-                else {
-                    bweekprogram.setText("Week program off");
-                    bweekprogram.setBackground(weekoff);
+                else {                                                           // current state of week program is on
+                    new Thread(new Runnable() {
+                         @Override
+                         public void run() {
+                         try {
+                            HeatingSystem.put("weekProgramState", "off");       // set program state to off
+                         } catch (Exception e) {
+                              System.err.println("Error from getdata " + e);
+                         }
+                    }}).start();
+                    bweekprogram.setText("Week program off");                   // set program button text to "on"
+                    bweekprogram.setBackground(weekoff);                        // set program button layout to off
                     on = false;
                 }
-
             }
         });
+
+        // server week program switch
+        getParamProgram = "";
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    getParamProgram  = HeatingSystem.get("weekProgramState");   // get program state
+                    currentTime.setText(getParamProgram);                       // set program state to string
+                    if (getParamProgram.equals("off")) {                        // if program state is "off"
+                        bweekprogram.setBackground(weekoff);                    // set program button layout to off
+                        bweekprogram.setText("Week program off");               // set program button text to "off"
+                        on = false;                                             // remember state
+                    } else {                                                    // if program state is "on"
+                        bweekprogram.setBackground(weekon);                     // set program button layout to on
+                        bweekprogram.setText("Week program on");                // set program button text to "on"
+                        on = true;                                              // remember state
+                    }
+                } catch (Exception e) {                                         // catch error, always add this !!
+                    System.err.println("Error from getdata "+e);
+                }
+            }
+        }).start();
+
+        // test server with time
+
+        currentTime = (TextView) findViewById(R.id.currentTime);/*
+        taskTime = new TimerTask() {
+            @Override
+            public void run() {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            getParamTime = HeatingSystem.get("time");
+                            currentTime.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    currentTime.setText(getParamTime);
+                                }
+                            });
+                        } catch (Exception e) {
+                            System.err.println("Error from getdata "+e);
+                        }
+                    }
+                }).start();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(taskTime, 0, clockTime);
+        */
     }
+
+    // freeze message
     public void freeze(View view) {
         AlertDialog.Builder freeze = new AlertDialog.Builder(this);
         freeze.setMessage("Do not freeze the pipes!! The minimum temperature is 5,0 \u2103")
@@ -131,6 +221,7 @@ public class ThermostatOverview extends AppCompatActivity {
                 .create();
         freeze.show();
     }
+    // burn message
     public void burn(View view) {
         AlertDialog.Builder burn = new AlertDialog.Builder(this);
         burn.setMessage("Do not burn the pipes!! The maximum temperature is 30,0 \u2103")
